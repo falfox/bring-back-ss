d8284b4249d4ba8d6c2a081d9dc7791d8dc60e52
Ridho Maulana <edoin21@gmail.com>
2024-05-21 23:38:43 +0700

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1,36 +1,16 @@
 let wpRequire;
+window.webpackChunkstudystream.push([[Math.random()], {}, (r) => { wpRequire = r; }]);
 
-let startingTime = new Date().getTime();
-
-function waitForWebpackChunkstudystream() {
-    if (window.webpackChunkstudystream) {
-
-        window.webpackChunkstudystream.push([[Math.random()], {}, (req) => {
-            wpRequire = req;
-        }]);
-
-        for (const key of Object.keys(wpRequire.m)) {
-            let fun = wpRequire.n(wpRequire(key))();
-
-            if (!fun || typeof fun !== "object")
-                continue;
-
-            for (const mod of Object.values(fun)) {
-                if (typeof mod === "function" && mod.hasOwnProperty('prototype') && mod.prototype.hasOwnProperty('adsDisabled')) {
-                    mod.prototype.adsDisabled = () => true;
-                    console.log("[Bypass Pin] Now you can pin up to the maximum of 20 users!");
-                }
-            }
-        }
-    } else {
-        if (new Date().getTime() - startingTime > 30000) {
-            console.log("[Bypass Pin] Timeout! Please refresh the page and try again.");
-            return;
-        }
-
-        console.log("[Bypass Pin] Waiting for webpackChunkstudystream to load...");
-        setTimeout(waitForWebpackChunkstudystream, 500); // Check again after 500 milliseconds
+for (const [k, m] of Object.entries(wpRequire.m)) {
+  let f = wpRequire.n(wpRequire(k))();
+  if (!f || typeof f !== "object") continue;
+  for (const mod of Object.values(f)) {
+    if (typeof mod === "function" && mod.hasOwnProperty('prototype') && mod.prototype.hasOwnProperty('adsDisabled')) {
+      Object.defineProperty(mod.prototype, 'adsDisabled', {
+        get: function() { return true; },
+        configurable: true
+      });
+      console.log("[Bypass Pin] Now you can pin up to the maximum of 20 users!");
     }
+  }
 }
-
-waitForWebpackChunkstudystream();
